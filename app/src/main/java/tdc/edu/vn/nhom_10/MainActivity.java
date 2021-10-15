1f4801e1a5eb7ad56d7356954aa778f036a80a15
nguyendienlam351 <nguyendienlam351@gmail.com>
2021-10-15 09:01:52 +0700

    Initial commit

---
+++ app/src/main/java/tdc/edu/vn/nhom_10/MainActivity.java
@@ -0,0 +1,14 @@
+package tdc.edu.vn.nhom_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
