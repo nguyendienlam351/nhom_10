92e851f6e9bdd25a10edd0789a2bf50bd67b3739
nguyendienlam351 <nguyendienlam351@gmail.com>
2021-10-18 08:03:21 +0700

    styles

--- app/src/main/java/tdc/edu/vn/nhom_10/MainActivity.java
+++ app/src/main/java/tdc/edu/vn/nhom_10/MainActivity.java
@@ -3,12 +3,48 @@ package tdc.edu.vn.nhom_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        setEvent();
+        setControl();
     }
+
+    private void setEvent() {
+        // Write a message to the database
+
+        button = findViewById(R.id.add);
+
+    }
+
+    private void setControl() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
 }
