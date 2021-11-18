e3f1aa1b783281257a565aec734a951e01b1bc0a
namnguyen231 <nguyenphuongnam.tdc2019@gmail.com>
2021-11-19 01:54:04 +0700

    Lan5

--- app/src/main/java/tdc/edu/vn/nhom_10/QuanLyFragment/NhapXuatKho.java
+++ app/src/main/java/tdc/edu/vn/nhom_10/QuanLyFragment/NhapXuatKho.java
@@ -16,7 +16,7 @@ import tdc.edu.vn.nhom_10.QuanLyBan;
 import tdc.edu.vn.nhom_10.R;
 
 public class NhapXuatKho extends Fragment {
-    Button btnNguyenLieu, btnNhapKho, btnXuatKho;
+    Button btnNguyenLieu, btnNhapKho, btnXuatKho, btnKiemKeKho;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,11 +34,33 @@ public class NhapXuatKho extends Fragment {
                 startActivity(intent);
             }
         });
+        btnXuatKho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DanhSachNguyenLieu.class);
+                startActivity(intent);
+            }
+        });
+        btnNhapKho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DanhSachNguyenLieu.class);
+                startActivity(intent);
+            }
+        });
+        btnKiemKeKho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DanhSachNguyenLieu.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setControl(View view) {
         btnNguyenLieu = view.findViewById(R.id.btnNguyenLieu);
         btnNhapKho = view.findViewById(R.id.btnNhapkho);
         btnXuatKho = view.findViewById(R.id.btnXuatkho);
+        btnKiemKeKho = view.findViewById(R.id.btnKiemKeKho);
     }
 }
